8426b00fae983d87cc26c4194a77ae7dda994857
shabnambegum911-hash <shabnambegum911@gmail.com>
2026-03-20 12:35:02 +0530

    making changes in cart service

--- ecom-ui/src/app/services/cart.service.ts
+++ ecom-ui/src/app/services/cart.service.ts
@@ -8,27 +8,28 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class CartService {
-  private apiUrl = `${environment.apiUrl}/cart`;
+  // Service for managing shopping cart operations
+  private cartApiUrl = `${environment.apiUrl}/cart`;
 
   constructor(private http: HttpClient) {}
 
   getCart(userId: number): Observable<Cart> {
-    return this.http.get<Cart>(`${this.apiUrl}/${userId}`);
+    return this.http.get<Cart>(`${this.cartApiUrl}/${userId}`);
   }
 
   addToCart(userId: number, productId: number, quantity: number): Observable<Cart> {
-    return this.http.post<Cart>(`${this.apiUrl}/${userId}/add?productId=${productId}&quantity=${quantity}`, {});
+    return this.http.post<Cart>(`${this.cartApiUrl}/${userId}/add?productId=${productId}&quantity=${quantity}`, {});
   }
 
   removeFromCart(userId: number, productId: number): Observable<Cart> {
-    return this.http.delete<Cart>(`${this.apiUrl}/${userId}/remove/${productId}`);
+    return this.http.delete<Cart>(`${this.cartApiUrl}/${userId}/remove/${productId}`);
   }
 
   updateCartItem(userId: number, productId: number, quantity: number): Observable<Cart> {
-    return this.http.put<Cart>(`${this.apiUrl}/${userId}/update/${productId}?quantity=${quantity}`, {});
+    return this.http.put<Cart>(`${this.cartApiUrl}/${userId}/update/${productId}?quantity=${quantity}`, {});
   }
 
   clearCart(userId: number): Observable<Cart> {
-    return this.http.delete<Cart>(`${this.apiUrl}/${userId}/clear`);
+    return this.http.delete<Cart>(`${this.cartApiUrl}/${userId}/clear`);
   }
 }
